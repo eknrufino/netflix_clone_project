60646aab15bd074445ed1921815aa399cc7e64cc
Eric Kazuo <erickazuo@Erics-MacBook-Air.local>
2023-12-13 19:13:56 -0300

    Core Data (Melhores Praticas)

--- Netflix_Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix_Clone/Views/CollectionViewTableViewCell.swift
@@ -56,7 +56,16 @@ class CollectionViewTableViewCell: UITableViewCell {
     }
     
     private func downloadTitleAt(indexPath: IndexPath){
-        print("Downloading \(titles[indexPath.row].original_title ?? "")")
+        
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                print("Downloaded to Database")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
     }
     
 }
@@ -111,5 +120,7 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         }
         return config
     }
+    
+    
     
 }
